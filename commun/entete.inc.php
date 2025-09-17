b0fd700a8eaac7dba6b64ec7d4af7266f154f765
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2025-09-17 12:06:11 -0400

    i18n : réorganisation du code pour suivre principes SoC

--- commun/entete.inc.php
+++ commun/entete.inc.php
@@ -1,62 +1,21 @@
 <?php
-/*********************** LECTURE DYNAMIQUE DES LANGUES DISPOS *********/
-$languesDisponibles = [];
-$contenuI18n = scandir("i18n");
-// Améliorer ce code (à l'aide de Copilot ou vos propres idées).
-foreach ($contenuI18n as $nomFichier) {
-    if($nomFichier != "." && $nomFichier != "..") {
-        $languesDisponibles[] = substr($nomFichier, 0, 2);
-    }
-}
+// Inclure le fichier de la librairie i18n
+include("lib/i18n.lib.php");
 
-/*********************** INTERACTIVITÉ DE CHANGEMENT DE LANGUE ********/
-    // Récupérer le tableau qui contient tous les paramètres de la requête HTTP (QUERYSTRING)
-    
-    // Langue par défaut
-    $langue = "fr";
+// Obtenir le tableau des langues disponibles
+$languesDisponibles = obtenirLanguesDisponibles();
 
-    // Si l'utilisateur a fait un choix de langue dans le passé, il faut 
-    // l'utiliser (donc il faut vérifier s'il y a un témoin HTTP nommé 
-    // "langueChoisie")
-    // Remarquez la condition additionnelle pour valider que la langue
-    // choisie est bien dans la liste des langues disponibles.
-    if(isset($_COOKIE["langueChoisie"]) && in_array($_COOKIE["langueChoisie"], $languesDisponibles)) {
-        $langue = $_COOKIE["langueChoisie"];
-    }
+// Déterminer la langue à utiliser
+$langue = determinerCodeLangue($languesDisponibles);
 
-    // Si l'utilisateur clique un des boutons de langues disponibles
-    // Remarquez la condition additionnelle pour valider que la langue
-    // choisie est bien dans la liste des langues disponibles.
-    if(isset($_GET["lan"]) && in_array($_GET["lan"], $languesDisponibles)) {
-        $langue = $_GET["lan"];
-        // Retenir le choix de langue dans un témoin HTTP (cookie)
-        // Date d'expiration dans le futur (3 mois)
-        setcookie("langueChoisie", $langue, time()+365*24*60*60);
-    }
-
-/********************** EXTERNALISATION DES TEXTES STATIQUES **********/
-    // Lire le fichier JSON qui contient les textes.
-    $textesChaineJson =  file_get_contents("i18n/".$langue.".json");
-    // Test
-    // echo $textesChaineJson;
-
-    // Convertir la chaîne de caractères JSON en objet (ou autre structure de données) PHP.
-    $textes = json_decode($textesChaineJson);
-    // Test
-    // echo $textes; // ERREUR : on ne peut pas "imprimer" une structure (comme un objet ou un tableau)
-    // Utiliser les méthodes print_r() ou var_dump() à la place : 
-    // print_r($textes);
-    // var_dump($textes)
-
-    // Quelques raccourcis utiles pour cet objet ($textes)
-    $_ent = $textes->entete;
-    $_pp = $textes->pied2page;
-
-    $_ = $textes->$page;
+// Obtenir les textes et les mettres dans les raccourcis utilisés dans le site.
+// On utilise l'affectation par épandage (destructuring assignment)
+[$_ent, $_pp, $_] = recupererTextesStatiques($langue, $page);
 
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -69,23 +28,23 @@ foreach ($contenuI18n as $nomFichier) {
     <link rel="stylesheet" href="css/styles.css">
     <link rel="icon" type="image/png" href="images/favicon.png" />
 </head>
+
 <body>
     <div class="conteneur">
         <header>
             <nav class="barre-haut">
                 <!-- Générer les boutons de choix de langue dynamiquement en PHP -->
-                
+
                 <?php
-                    // Méthode 1 : moins désirable
-                    // foreach ($languesDisponibles as $codeLangue) {
-                    //     echo "<a href='?lan=$codeLangue'>$codeLangue</a>";
-                    // }
+                // Méthode 1 : moins désirable
+                // foreach ($languesDisponibles as $codeLangue) {
+                //     echo "<a href='?lan=$codeLangue'>$codeLangue</a>";
+                // }
                 ?>
-                <?php foreach($languesDisponibles as $codeLangue) :  ?>
-                    <a 
-                        <?= $langue==$codeLangue ? 'class="actif"' : '' ?>
-                        href="?lan=<?= $codeLangue; ?>"
-                    >
+                <?php foreach ($languesDisponibles as $codeLangue) :  ?>
+                    <a
+                        <?= $langue == $codeLangue ? 'class="actif"' : '' ?>
+                        href="?lan=<?= $codeLangue; ?>">
                         <?= $codeLangue; ?>
                     </a>
                 <?php endforeach ?>
@@ -99,11 +58,11 @@ foreach ($contenuI18n as $nomFichier) {
             <input type="checkbox" id="cc-btn-responsive">
             <nav class="principale">
                 <label for="cc-btn-responsive" class="menu-controle material-icons">close</label>
-                <a <?= $page=="teeshirts" ? ' class="actif" ' : '' ?> href="teeshirts.php"><?= $_ent->menuTeeshirts; ?></a>
-                <a <?= $page=="casquettes" ? ' class="actif" ' : '' ?> href="casquettes.php"><?= $_ent->menuCasquettes; ?></a>
-                <a <?= $page=="hoodies" ? ' class="actif" ' : '' ?> href="hoodies.php"><?= $_ent->menuHoodies; ?></a>
+                <a <?= $page == "teeshirts" ? ' class="actif" ' : '' ?> href="teeshirts.php"><?= $_ent->menuTeeshirts; ?></a>
+                <a <?= $page == "casquettes" ? ' class="actif" ' : '' ?> href="casquettes.php"><?= $_ent->menuCasquettes; ?></a>
+                <a <?= $page == "hoodies" ? ' class="actif" ' : '' ?> href="hoodies.php"><?= $_ent->menuHoodies; ?></a>
                 <span class="separateur"></span>
-                <a <?= $page=="aide" ? ' class="actif" ' : '' ?> href="aide.php"><?= $_ent->menuAide; ?></a>
-                <a <?= $page=="apropos" ? ' class="actif" ' : '' ?> href="apropos.php"><?= $_ent->menuNous; ?></a>
+                <a <?= $page == "aide" ? ' class="actif" ' : '' ?> href="aide.php"><?= $_ent->menuAide; ?></a>
+                <a <?= $page == "apropos" ? ' class="actif" ' : '' ?> href="apropos.php"><?= $_ent->menuNous; ?></a>
             </nav>
         </header>
---
+++ lib/i18n.lib.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Librairie des fonctions associées au module d'internationnalisation (i18n)
+ */
+
+/**
+ * Obtenir les langues disponibles.
+ * 
+ * @return array Tableau des codes (2 lettres) de langues disponibles.
+ */
+function obtenirLanguesDisponibles()
+{
+    $langues = [];
+    $contenuI18n = scandir("i18n");
+    // Améliorer ce code (à l'aide de Copilot ou vos propres idées).
+    foreach ($contenuI18n as $nomFichier) {
+        if ($nomFichier != "." && $nomFichier != "..") {
+            $langues[] = substr($nomFichier, 0, 2);
+        }
+    }
+
+    return $langues;
+}
+
+/**
+ * Déterminer le code de la langue active dans le site.
+ * 
+ * @param array $codeLangues Tableau des codes de langues disponibles.
+ * 
+ * @return string Chaîne représentant le code de la langue.
+ */
+function determinerCodeLangue($codesLangues)
+{
+    // Langue par défaut
+    $langue = "fr";
+
+    // Langue présélectionnée dans un cookie
+    if (isset($_COOKIE["langueChoisie"]) && in_array($_COOKIE["langueChoisie"], $codesLangues)) {
+        $langue = $_COOKIE["langueChoisie"];
+    }
+    // Langue choisie par l'utilisateur dans la barre de navigation
+    if (isset($_GET["lan"]) && in_array($_GET["lan"], $codesLangues)) {
+        $langue = $_GET["lan"];
+        setcookie("langueChoisie", $langue, time() + 365 * 24 * 60 * 60);
+    }
+
+    return $langue;
+}
+
+/**
+ * Obtenir les textes statiques pour l'entête, le pied de page et la page spécifique.
+ * 
+ * @param string $codeLangue Code de la langue dans laquelle on veut les textes.
+ * @param string $page Étiquette associée avec la page affichée.
+ * 
+ * @return array Tableau contenant 3 objets (entête, pied de page, page spécifique).
+ */
+function recupererTextesStatiques($codeLangue, $page) {
+    $textesChaineJson =  file_get_contents("i18n/" . $codeLangue . ".json");
+    $textes = json_decode($textesChaineJson);
+    return [$textes->entete, $textes->pied2page, $textes->$page];
+}
