e07a68c739fe41b21c4c42c2a91b2e7c4858eea6
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2025-10-07 10:19:40 -0400

    Fichier teeshirts.json complété avec thèmes et autres propriétés.

--- teeshirts.php
+++ teeshirts.php
@@ -8,12 +8,37 @@ include("commun/entete.inc.php");
 $produits = json_decode(file_get_contents("data/teeshirts.json"));
 ?>
 <main class="page-produits page-teeshirts">
-    <article class="amorce">
-        <h1><?= $_->titre; ?></h1>
-    </article>
-    <article class="principal">
-        <!-- Gabarit -->
-		<?php foreach($produits as $prd) : ?>
+	<article class="amorce">
+		<h1><?= $_->titre; ?></h1>
+		<!-- Barre de contrôle (filtrage, tri, etc.) -->
+		<form class="controle">
+			<div class="filtre">
+				<label for="filtre">Filtrer par thème : </label>
+				<select name="filtre" id="filtre">
+					<option value="tous">Tous les produits</option>
+					<option value="animaux">Animaux</option>
+					<option value="nature">Nature</option>
+					<option value="jeux">Jeux vidéo</option>
+					<option value="inusite">Inusité</option>
+					<option value="sport">Sport</option>
+				</select>
+			</div>
+			<div class="tri">
+				<label for="tri">Trier par : </label>
+				<select name="tri" id="tri">
+					<option value="tous">Meilleur vendeur</option>
+					<option value="prix-asc">Prix / ascendant</option>
+					<option value="prix-desc">Prix / descendant</option>
+					<option value="nom-asc">Alpha / ascendant</option>
+					<option value="nom-desc">Alpha / descendant</option>
+					<option value="date_jout">Nouveauté</option>
+				</select>
+			</div>
+		</form>
+	</article>
+	<article class="principal">
+		<!-- Gabarit -->
+		<?php foreach ($produits as $prd) : ?>
 			<div class="produit">
 				<span class="image">
 					<img src="images/produits/teeshirts/<?= $prd->id; ?>.webp" alt="<?= $prd->nom; ?>">
@@ -22,7 +47,7 @@ $produits = json_decode(file_get_contents("data/teeshirts.json"));
 				<span class="prix"><?= number_format($prd->prix, 2, ',', ' '); ?> $</span>
 			</div>
 		<?php endforeach; ?>
-    </article>
+	</article>
 </main>
 <?php
 // Inclure la partie commune en bas de page.
