87ebda54c6f4b6ae079e085ef181fac066e0306f
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2025-11-11 10:41:13 -0500

    Conception terminée. Implémentation démarrée.

---
+++ data/implementation.sql
@@ -0,0 +1,76 @@
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'categorie'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `categorie`;
+		
+CREATE TABLE `categorie` (
+  `id` TINYINT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'produit'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `produit`;
+		
+CREATE TABLE `produit` (
+  `id` SMALLINT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(200) NOT NULL,
+  `ventes` SMALLINT NOT NULL DEFAULT 0,
+  `dac` DATE NOT NULL,
+  `prix` DECIMAL(5,2) NOT NULL,
+  `thm_id` TINYINT NOT NULL,
+  `cat_id` TINYINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'theme'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `theme`;
+		
+CREATE TABLE `theme` (
+  `id` TINYINT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `produit` ADD FOREIGN KEY (thm_id) REFERENCES `theme` (`id`);
+ALTER TABLE `produit` ADD FOREIGN KEY (cat_id) REFERENCES `categorie` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `categorie` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `produit` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `theme` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `categorie` (`id`,`nom`) VALUES
+-- ('','');
+-- INSERT INTO `produit` (`id`,`nom`,`ventes`,`dac`,`prix`,`thm_id`,`cat_id`) VALUES
+-- ('','','','','','','');
+-- INSERT INTO `theme` (`id`,`nom`) VALUES
+-- ('','');
