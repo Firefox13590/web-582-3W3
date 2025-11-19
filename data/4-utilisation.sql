58537849d818775e170f3a1c0f808a58fb27d9a6
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2025-11-19 09:30:45 -0500

    Exemple d'intégration PHP complété.

---
+++ data/4-utilisation.sql
@@ -0,0 +1,13 @@
+-- Examples de toutes les commandes CRUD
+-- c-a-d : INSERT/SELECT/UPDATE/DELETE
+
+-- A) Commande INSERT
+
+
+-- B) Commande SELECT
+
+
+-- C) Commande UPDATE
+
+
+-- D) Commande DELETE
--- data/5-integration.php
+++ data/5-integration.php
@@ -7,37 +7,43 @@
 </head>
 <body>
     <h2>Thèmes disponibles dans le catalogue des produits TeeTIM</h2>
-    <ul>
+    <ol>
     <?php
         // Se connecter au serveur MySQL
         // On utilise dans cet exemple la librairie PHP MySQLi
         
         // Étape 1 : se connecter au serveur MySQL
-        $cnx = mysqli_connect("localhost", "root", "");
+        $cnx = mysqli_connect("127.0.0.1", "root", "");
+        // Définir l'encodage de caractères
+        mysqli_set_charset($cnx, 'utf8mb4');
 
         // Étape 2 : sélectionner la BD
         mysqli_select_db($cnx, "teetim_gr2");
 
         // Étape 3 : on écrit une requête SQL dans une chaîne de caractères
-        $requeteSql = "SELECT nom FROM theme";
+        $requeteSql = "SELECT * FROM produit";
 
         // Étape 4 : on exécute la requête sur la connexion disponible et 
         // on récolte le résultat ("jeu d'enregistrements")
-        $resultat = mysqli_query($cnx, $requeteSql);
+        $jeuEnregistrements = mysqli_query($cnx, $requeteSql);
+        // print_r($jeuEnregistrements);
 
         // Étape 5 : travailler avec ce résultat
         // Méthode 1 : sortir toutes les lignes (enregistrement) d'un seul coup 
         // du jeu d'enregistrements et les mettre dans un tableau PHP
-        $enregistrements = mysqli_fetch_all($resultat); 
+        $tabLignes = mysqli_fetch_all($jeuEnregistrements, MYSQLI_ASSOC); 
+        // print_r($tabLignes);
 
         // print_r($enregistrements);
-        foreach($enregistrements as $ligne) {
+        foreach($tabLignes as $ligne) {
             echo "<li>";
-                echo $ligne[0];
+                echo $ligne["nom"];
+                echo ", ".$ligne["prix"];
+                echo ", ".$ligne["id"];
             echo "</li>";
         }
 
     ?>
-    </ul>
+    </ol>
 </body>
 </html>
